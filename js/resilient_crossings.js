1050b0507c6f9348923c7ac121da9708ceb08358
VanessaJO <vobrien3@student.cccs.edu>
2018-04-22 14:33:44 -0600

    add basemap toggle
    although broken, basemap toggle added

--- js/resilient_crossings.js
+++ js/resilient_crossings.js
@@ -1,7 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var map = L.map("map").setView([40.03, -105.42], 13);
+    var map = L.map("map").setView([40.03, -105.42], 13); // ([lat, lon], initial zoom level)
 
-    L.esri.basemapLayer("Topographic").addTo(map);
+    var layer = L.esri.basemapLayer("Topographic").addTo(map);
+
+    var layerLabels;
+
+    // function to switch basemaps
+    function setBasemap(basemap) {
+        if (layer) {
+            map.removeLayer(layer);
+        }
+
+        layer = L.esri.basemapLayer(basemap);
+
+        map.addLayer(layer);
+
+        if (layerLabels) {
+            map.removeLayer(layerLabels);
+        }
+
+        if (basemap === 'ShadedRelief'
+            || basemap === 'Oceans'
+            || basemap === 'Gray'
+            || basemap === 'DarkGray'
+            || basemap === 'Imagery'
+            || basemap === 'Terrain'
+           ) {
+            layerLabels = L.esri.basemapLayer(basemap + 'Labels');
+            map.addLayer(layerLabels);
+        }
+    }
+
+    function changeBasemap(basemaps){
+        var basemap = basemaps.value;
+        setBasemap(basemap);
+    }
 
     // add a map service: Streams and Ditches from Boulder County
     var streams = L.esri.featureLayer({

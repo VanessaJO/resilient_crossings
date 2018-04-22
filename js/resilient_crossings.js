14c83f082677c3cd600cc5cc61fa0806d90a66e7
VanessaJO <vobrien3@student.cccs.edu>
2018-04-21 22:59:45 -0600

    style header & footer
    style header and footer, add link to fourmile webpage

--- js/resilient_crossings.js
+++ js/resilient_crossings.js
@@ -3,22 +3,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
     L.esri.basemapLayer("Topographic").addTo(map);
 
-    // add a map service such as streams and ditches from Boulder County
+    // add a map service: Streams and Ditches from Boulder County
     var streams = L.esri.featureLayer({
         url: 'http://maps.bouldercounty.org/arcgis/rest/services/HYDRO/STREAMS_DITCHES/MapServer/0',
     }).addTo(map);
 
-    // Fourmile Creek Watershed
+    // add a feature service: Fourmile Creek Watershed
     var waterShed = L.esri.featureLayer({
         url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/2',
     }).addTo(map);
 
-    // Fire Stations
+    // add a feature service: Fire Stations
     var fireStations = L.esri.featureLayer({
         url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/1',
     }).addTo(map);
 
-    // Bridges and Culverts
+    // add a feature service: Bridges and Culverts
     var bridgesCulverts = L.esri.featureLayer({
         url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/0',
     }).addTo(map);

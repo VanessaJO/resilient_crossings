8d2a5872e89da5fc181b69fae50dc927e5df54ab
VanessaJO <vobrien3@student.cccs.edu>
2018-04-23 03:17:54 -0600

    add popups to feature layers
    add popups to several feature layers, update esri-leaflet version in <head> from 2.1.3 to 2.1.4, change streams feature layer to a dynamic map layer

--- js/resilient_crossings.js
+++ js/resilient_crossings.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var map = L.map("map").setView([40.03, -105.42], 13); // ([lat, lon], initial zoom level)
+    var map = L.map('map').setView([40.03, -105.42], 13); // ([lat, lon], initial zoom level)
 
-    var layer = L.esri.basemapLayer("Topographic").addTo(map);
+    var layer = L.esri.basemapLayer('Topographic').addTo(map);
 
     var layerLabels;
 
@@ -37,9 +37,23 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // add a map service: Streams and Ditches from Boulder County
-    var streams = L.esri.featureLayer({
-        url: 'http://maps.bouldercounty.org/arcgis/rest/services/HYDRO/STREAMS_DITCHES/MapServer/0',
+    var streams = L.esri.dynamicMapLayer({
+        url: 'http://maps.bouldercounty.org/arcgis/rest/services/HYDRO/STREAMS_DITCHES/MapServer',
     }).addTo(map);
+
+
+// customize a popup on a dynamic map layer: Streams and Ditches
+    streams.bindPopup(function (error, featureCollection) {
+        if(error || featureCollection.features.length === 0) {
+            return false;
+        } else {
+            return 'Risk Level: ' + featureCollection.features[0].properties.CLASS_DESC;
+        }
+    });
+
+    streams.bindPopup(function (layer) {
+        return L.Util.template('<p>{Name}<br>{Feature Type}<br>{Water Source}</p>', layer.feature.properties);
+    });
 
     // add a feature service: Fourmile Creek Watershed
     var waterShed = L.esri.featureLayer({
@@ -51,9 +65,19 @@ document.addEventListener('DOMContentLoaded', function() {
         url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/1',
     }).addTo(map);
 
+    // customize popups on a feature layer: Fire Stations
+    fireStations.bindPopup(function (layer) {
+        return L.Util.template('<p>District: {District}<br>Chief: {Chief}<br>Website: {Website1}<br>Address: {Address}<br>Captain: {Captain}<br>Phone: {Phone}<br>Paid Employees: {Paid_Emplo}<br>Volunteers: {Volunteers}<br>Station Name: {Station_Na}<br>Equipment: {Equipment}<br></p>', layer.feature.properties);
+    });
+
     // add a feature service: Bridges and Culverts
     var bridgesCulverts = L.esri.featureLayer({
         url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/0',
     }).addTo(map);
+
+    // customize popups on a feature layer: Bridges and Culverts
+    bridgesCulverts.bindPopup(function (layer) {
+        return L.Util.template('<p>Address: {Address}<br>Access Type: {AccessType}<br>Material: {ConstType}<br>Width: {Width}<br>Span: {Span}<br>Railings: {Railings}<br>Wt Limit: {WtLimit}<br>Damage: {Damage}<br>Response: {Response}</p>', layer.feature.properties);
+    });
 
 });

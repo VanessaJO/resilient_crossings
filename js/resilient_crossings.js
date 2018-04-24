0a12160c69828e43bac5a418e822bc9758b74f00
VanessaJO <vobrien3@student.cccs.edu>
2018-04-23 19:20:41 -0600

    just little stuff
    grasping at straws to see if anything helps my widget run

--- js/resilient_crossings.js
+++ js/resilient_crossings.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var map = L.map('map').setView([40.03, -105.42], 13); // ([lat, lon], initial zoom level)
+document.addEventListener('DOMContentLoaded', function () {
+    var map = L.map("map").setView([40.03, -105.42], 13); // ([lat, lon], initial zoom level)
 
     var layer = L.esri.basemapLayer('Topographic').addTo(map);
 
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    function changeBasemap(basemaps){
+    function changeBasemap(basemaps) {
         var basemap = basemaps.value;
         setBasemap(basemap);
     }
@@ -39,30 +39,30 @@ document.addEventListener('DOMContentLoaded', function() {
     // add a map service: Streams and Ditches from Boulder County
     var streams = L.esri.dynamicMapLayer({
         url: 'http://maps.bouldercounty.org/arcgis/rest/services/HYDRO/STREAMS_DITCHES/MapServer',
+        opacity: 0.55
     }).addTo(map);
 
-
-// customize a popup on a dynamic map layer: Streams and Ditches
+    // customize a popup on a dynamic map layer: Streams and Ditches
     streams.bindPopup(function (error, featureCollection) {
-        if(error || featureCollection.features.length === 0) {
+        if (error || featureCollection.features.length === 0) {
             return false;
         } else {
-            return 'Risk Level: ' + featureCollection.features[0].properties.CLASS_DESC;
+            return 'Name: ' + featureCollection.features[0].properties.Name;
         }
     });
 
-    streams.bindPopup(function (layer) {
-        return L.Util.template('<p>{Name}<br>{Feature Type}<br>{Water Source}</p>', layer.feature.properties);
-    });
+    /*
+    How would I append Feature Type and Water Source fields to the streams popup above so that it would return all 3 values?
+    */
 
     // add a feature service: Fourmile Creek Watershed
     var waterShed = L.esri.featureLayer({
-        url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/2',
+        url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/2'
     }).addTo(map);
 
     // add a feature service: Fire Stations
     var fireStations = L.esri.featureLayer({
-        url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/1',
+        url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/1'
     }).addTo(map);
 
     // customize popups on a feature layer: Fire Stations
@@ -72,7 +72,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // add a feature service: Bridges and Culverts
     var bridgesCulverts = L.esri.featureLayer({
-        url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/0',
+        url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/0'
     }).addTo(map);
 
     // customize popups on a feature layer: Bridges and Culverts

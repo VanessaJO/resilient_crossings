d83aa6eb0c67ff64b14e1fa4ad2b09328d947e56
VanessaJO <vobrien3@student.cccs.edu>
2018-04-25 19:28:19 -0600

    just little things in js
    just little things in js

--- js/resilient_crossings.js
+++ js/resilient_crossings.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var map = L.map("map").setView([40.03, -105.42], 11); // ([lat, lon], initial zoom level)
+    var map = L.map('map').setView([40.03, -105.42], 11); // ([lat, lon], initial zoom level)
 
     var layer = L.esri.basemapLayer('Topographic').addTo(map);
 
     var layerLabels;
 
-    // a button click and function to center around Fourmile Canyon
+    // A1) a button click and function to center around Fourmile Canyon
     document.getElementById("fourmileButton").onclick = function(){
         map.setView([40.03, -105.42], 13);
     };
 
-    // function to switch basemaps
+
+    // B1) function to switch basemaps
     function setBasemap(basemap) {
         if (layer) {
             map.removeLayer(layer);
@@ -36,18 +37,20 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    function changeBasemap(basemaps) {
+    function changeBasemap(basemaps){
+        console.log(basemaps.value);
         var basemap = basemaps.value;
         setBasemap(basemap);
     }
 
-    // A1) add a map service: Streams and Ditches from Boulder County
+
+    // C1) add a map service: Streams and Ditches from Boulder County
     var streams = L.esri.dynamicMapLayer({
         url: 'http://maps.bouldercounty.org/arcgis/rest/services/HYDRO/STREAMS_DITCHES/MapServer',
         opacity: 0.55
     }).addTo(map);
 
-    // A2) customize a popup on a dynamic map layer: Streams and Ditches
+    // C2) customize a popup on a dynamic map layer: Streams and Ditches
     streams.bindPopup(function (error, featureCollection) {
         if (error || featureCollection.features.length === 0) {
             return false;
@@ -56,45 +59,49 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // B1) add a feature service: Fourmile Creek Watershed
+
+    // D1) add a feature service: Fourmile Creek Watershed
     var waterShed = 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/2';
     L.esri.featureLayer({
         url: waterShed
     }).addTo(map);
 
-    // C1) add a feature service: Fire Stations
+
+    // E1) add a feature service: Fire Stations
     var fireStations = L.esri.featureLayer({
         url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/1'
     }).addTo(map);
 
-    // C2) customize popup for Fire Stations feature layer
+    // E2) customize popup for Fire Stations feature layer
     var popupFireStations = '<strong>{Station_Na}</strong><br>{Address}<br>{Phone}<br><small>Chief: {Chief}<br>Captain: {Captain}<br>Paid Employees: {Paid_Emplo}<br>Volunteers: {Volunteers}<br>Equipment: {Equipment}<small>';
 
-    // C3) bind popup for Fire Stations feature layer
+    // E3) bind popup for Fire Stations feature layer
     fireStations.bindPopup(function(e) {
         return L.Util.template(popupFireStations, e.feature.properties)
     });
 
-    // D1) add a feature service: Bridges and Culverts
+
+    // F1) add a feature service: Bridges and Culverts
     var bridgesCulverts = L.esri.featureLayer({
         url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/0'
     }).addTo(map);
 
-    // D2) customize popup for Bridges and Culverts feature layer
+    // F2) customize popup for Bridges and Culverts feature layer
     var popupBridgesCulverts = '<strong>{AccessType}</strong><br>{Address}<br>Damage: {Damage}<br>Response: {Response}<br><small>Material: {ConstType}<br>Width: {Width}<br>Span: {Span}<br>Railings: {Railings}<br>Wt Limit: {WtLimit}<small>';
 
-    // D3) bind popup for Bridges and Culverts feature layer
+    // F3) bind popup for Bridges and Culverts feature layer
     bridgesCulverts.bindPopup(function(e) {
         return L.Util.template(popupBridgesCulverts, e.feature.properties)
     });
 
-    // E1) GeoCode: create the geocoding control and add it to the map
+
+    // G1) GeoCode: create the geocoding control and add it to the map
     var searchControl = L.esri.Geocoding.geosearch().addTo(map);
 
-    // E2) GeoCode: create an empty layer group to store the results and add it to the map
+    // G2) GeoCode: create an empty layer group to store the results and add it to the map
     var results = L.layerGroup().addTo(map);
 
-    // E3) GeoCode: listen for the results event and add every result to the map
+    // G3) GeoCode: listen for the results event and add every result to the map
     searchControl.on("results", function(data) {
         results.clearLayers();
         for (var i = data.results.length - 1; i >= 0; i--) {
@@ -102,7 +109,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // F1) The Geosearch control can also search for results from a variety of sources including Feature Layers and Map Services. This is done with plain text matching and is not "real" geocoding. But it allows you to mix custom results into a search box.
+
+    // H1) The Geosearch control can also search for results from a variety of sources including Feature Layers and Map Services. This is done with plain text matching and is not "real" geocoding. But it allows you to mix custom results into a search box.
     var arcgisOnline = L.esri.Geocoding.arcgisOnlineProvider({
         url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/ArcGIS/rest/services/Fourmile_Creek_Watershed/FeatureServer/0',
         searchFields: ['AccessType', 'Damage'], // Search these fields for text matches
